4fe28ce3a218a6c64a2c5b181541b8fab776f5a5
Hermes <hermeschigoziri@gmail.com>
2022-07-13 12:13:06 +0100

    Build home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './css/App.css';
+import { Link } from "react-router-dom";
+import "./css/App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>Shopping Cart</p>
+      <nav>
+        <Link to={"/discover"}>Discover</Link>
+        <Link to={"/store"}>Store</Link>
+      </nav>
+
+      <div className="content">
+        <h1>HERMES</h1>
+        <p className="desc">Steam Keys and more</p>
+        <Link className="btn discover-btn" to={"/discover"}>Discover</Link>
+      </div>
     </div>
   );
 }
 
 export default App;
+
--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Discover from "./components/Discover";
 
 const RouteSwitch = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<App />} /> 
+                <Route path="/" element={<App />} />
+                <Route path="discover" element={<Discover />} />
+                <Route path="*" element={
+                <main>
+                    <p>There's nothing here.</p>
+                </main>} />
             </Routes>
         </BrowserRouter>
     );
