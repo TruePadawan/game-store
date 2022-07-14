6f57de628b177569fe556dbef9c0c686c76fddbf
Hermes <hermeschigoziri@gmail.com>
2022-07-14 14:04:59 +0100

    update component import location

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Discover from "./components/Discover";
+import Discover from "./components/pages/Discover";
 
 const RouteSwitch = () => {
     return (
